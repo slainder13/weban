d568f447eacc677438fd4f4263fc64c382889107
slainder13 <108572375+slainder13@users.noreply.github.com>
2023-11-19 01:09:12 +0100

    Update weban.py

--- weban.py
+++ weban.py
@@ -1,8 +1,8 @@
 import os
 import sys
 sys.path.append('__init__')
-from __init__ import get_ip as ad
-from __init__ import file_mg as file
+import get_ip as ad
+import file_mg as file
 from art import *
 from termcolor import colored
 
